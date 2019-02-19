da5d126ef18fd55c4a4ac4ef0dd468459cb63519
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2019-02-19 11:52:10 +0300

    chore: update dependencies

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = {
   extends: [
-    "eslint-config-airbnb-base",
-    "eslint-config-airbnb-base/rules/strict",
-    "eslint-config-prettier"
+    'eslint-config-airbnb-base',
+    'eslint-config-airbnb-base/rules/strict',
+    'eslint-config-prettier'
   ].map(require.resolve),
   rules: {
-    "arrow-parens": ["warn", "as-needed", { requireForBlockBody: true }],
-    "comma-dangle": ["error", "never"],
-    "jsx-quotes": ["error", "prefer-single"],
-    "max-len": ["error", { code: 80, ignoreUrls: true }],
-    quotes: ["error", "single"]
+    'arrow-parens': ['warn', 'as-needed', { requireForBlockBody: true }],
+    'comma-dangle': ['error', 'never'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'max-len': ['error', { code: 80, ignoreUrls: true }],
+    quotes: ['error', 'single']
   }
 };
