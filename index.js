ac8af27fc1eb22d497185ba097c51c4b0909f44c
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2019-02-19 14:19:05 +0300

    feat: add React config

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 module.exports = {
   extends: [
-    'eslint-config-airbnb',
-    'eslint-config-prettier',
-    'eslint-config-urbica/defaults'
+    'eslint-config-airbnb-base',
+    'eslint-config-airbnb-base/rules/strict',
+    'eslint-config-prettier'
   ].map(require.resolve),
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/jsx-no-bind': 'error',
-    'react/prop-types': [1, { skipUndeclared: true }]
+    'arrow-parens': ['warn', 'as-needed', {requireForBlockBody: true}],
+    'comma-dangle': ['error', 'never'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'max-len': ['error', {code: 80, ignoreUrls: true}],
+    quotes: ['error', 'single']
   }
 };
---
+++ react.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'eslint-config-airbnb',
+    'eslint-config-prettier',
+    './index'
+  ].map(require.resolve),
+  rules: {
+    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+    'react/jsx-no-bind': 'error',
+    'react/prop-types': [1, {skipUndeclared: true}]
+  }
+};
