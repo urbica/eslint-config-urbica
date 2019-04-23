256a15077c8c3bd1a125c53632157b51491b9f3d
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2019-04-23 12:35:08 +0300

    feat: 🎸 add eslint-plugin-react-hooks
    BREAKING CHANGE: 🧨 bamp babel-eslint

--- react.js
+++ react.js
@@ -3,9 +3,14 @@ module.exports = {
     'eslint-config-airbnb',
     './index'
   ].map(require.resolve),
+  plugins: [
+    'react-hooks'
+  ],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-no-bind': 'error',
-    'react/prop-types': [1, { skipUndeclared: true }]
+    'react/prop-types': [1, { skipUndeclared: true }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   }
 };
